7eadd92d1f2588cfe10e16885bfb7dab50e7d0ae
sourav-bz <s.kumar001997@gmail.com>
2025-03-18 17:50:36 +0530

    player input controls added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::math::vec3;
 use bevy::prelude::*;
 
 const WW: f32 = 1200.0;
 const WH: f32 = 700.0;
 const BG_COLOR: (u8, u8, u8) = (197, 204, 184);
 
+//sprites
 const SPRITE_SHEET_PATH: &str = "assets.png";
 const SPRITE_SCALE_FACTOR: f32 = 3.0;
 const TILE_W: u32 = 16;
@@ -12,14 +14,20 @@ const TILE_H: u32 = 16;
 const SPRITE_SHEET_W: u32 = 8;
 const SPRITE_SHEET_H: u32 = 8;
 
+//player
+const PLAYER_SPEED: f32 = 2.0;
+
 //resources
 #[derive(Resource)]
 struct GlobalTextureAtlasHandle(Option<Handle<TextureAtlasLayout>>);
 #[derive(Resource)]
 struct GlobalSpriteSheetHandle(Option<Handle<Image>>);
 
+//components
+#[derive(Component)]
+struct Player;
 
-
+//state
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 enum GameState {
     #[default]
@@ -55,6 +63,7 @@ fn main() {
         //systems
         .add_systems(OnEnter(GameState::Loading), load_assets)
         .add_systems(OnEnter(GameState::GameInit), (setup_camera, init_world))
+        .add_systems(Update, handle_player_input.run_if(in_state(GameState::InGame)))
         .run();
 }
 
@@ -75,7 +84,12 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn((Camera2d, Camera{..Default::default()}));
 }
 
-fn init_world(mut commands: Commands, texture_atlas: Res<GlobalTextureAtlasHandle>, image_handle: Res<GlobalSpriteSheetHandle>){
+fn init_world(
+    mut commands: Commands, 
+    texture_atlas: Res<GlobalTextureAtlasHandle>, 
+    image_handle: Res<GlobalSpriteSheetHandle>,
+    mut next_state: ResMut<NextState<GameState>>
+){
     commands.spawn((
         Sprite::from_atlas_image(
             image_handle.0.clone().unwrap(),
@@ -85,5 +99,42 @@ fn init_world(mut commands: Commands, texture_atlas: Res<GlobalTextureAtlasHandl
             },
         ),
         Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
+        Player
     ));
+    next_state.set(GameState::InGame);
+}
+
+fn handle_player_input(mut query: Query<&mut Transform, With<Player>>, keyboard_input: Res<ButtonInput<KeyCode>>){
+
+    if query.is_empty(){
+        return;
+    }
+
+    let mut transform = query.single_mut();
+    let w_key = keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp);
+    let s_key = keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown);
+    let a_key = keyboard_input.pressed(KeyCode::KeyA) || keyboard_input.pressed(KeyCode::ArrowLeft);
+    let d_key = keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight);
+    
+    let mut delta = Vec2::ZERO;
+
+    if w_key{
+        delta.y += 1.0;
+    }
+    if s_key{
+        delta.y -= 1.0;
+    }
+    if a_key{
+        delta.x -= 1.0;
+    }
+    if d_key{
+        delta.x += 1.0;
+    }
+
+    delta = delta.normalize();
+
+    if delta.is_finite() && (w_key || s_key || a_key || d_key){
+        transform.translation += vec3(delta.x, delta.y, 0.0) * PLAYER_SPEED;
+    }
+    
 }
