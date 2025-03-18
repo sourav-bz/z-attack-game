29ae5cbaf223c61a2ac1c659f925c3547df70255
sourav-bz <s.kumar001997@gmail.com>
2025-03-18 17:32:24 +0530

    implemented game state

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,22 @@ const TILE_H: u32 = 16;
 const SPRITE_SHEET_W: u32 = 8;
 const SPRITE_SHEET_H: u32 = 8;
 
+//resources
+#[derive(Resource)]
+struct GlobalTextureAtlasHandle(Option<Handle<TextureAtlasLayout>>);
+#[derive(Resource)]
+struct GlobalSpriteSheetHandle(Option<Handle<Image>>);
+
+
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+enum GameState {
+    #[default]
+    Loading,
+    GameInit,
+    InGame,
+}
+
 fn main() {
     App::new()
         .add_plugins(
@@ -27,29 +43,44 @@ fn main() {
                     ..default()
                 }),
         )
+        .init_state::<GameState>()
+        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin)
         .insert_resource(ClearColor(Color::srgb_u8(
             BG_COLOR.0, BG_COLOR.1, BG_COLOR.2,
         )))
-        .add_plugins(LogDiagnosticsPlugin::default())
-        .add_plugins(FrameTimeDiagnosticsPlugin)
-        .add_systems(Startup, (setup_camera, spawn_player))
+        //custom resources
+        .insert_resource(GlobalTextureAtlasHandle(None))
+        .insert_resource(GlobalSpriteSheetHandle(None))
+        //systems
+        .add_systems(OnEnter(GameState::Loading), load_assets)
+        .add_systems(OnEnter(GameState::GameInit), (setup_camera, init_world))
         .run();
+}
+
+fn load_assets(
+    mut texture_atlas: ResMut<GlobalTextureAtlasHandle>, 
+    mut image_handle: ResMut<GlobalSpriteSheetHandle>, 
+    asset_server: Res<AssetServer>, 
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    mut next_state: ResMut<NextState<GameState>>
+){
+    image_handle.0 = Some(asset_server.load(SPRITE_SHEET_PATH));
+    let layout = TextureAtlasLayout::from_grid(UVec2::new(TILE_W, TILE_H), SPRITE_SHEET_W, SPRITE_SHEET_H, None, None);
+    texture_atlas.0 = Some(texture_atlas_layouts.add(layout));
+    next_state.set(GameState::GameInit);
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn((Camera2d, Camera{..Default::default()}));
 }
 
-fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>){
-    let texture = asset_server.load(SPRITE_SHEET_PATH);
-    let layout = TextureAtlasLayout::from_grid(UVec2::new(TILE_W, TILE_H), SPRITE_SHEET_W, SPRITE_SHEET_H, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-
+fn init_world(mut commands: Commands, texture_atlas: Res<GlobalTextureAtlasHandle>, image_handle: Res<GlobalSpriteSheetHandle>){
     commands.spawn((
         Sprite::from_atlas_image(
-            texture,
+            image_handle.0.clone().unwrap(),
             TextureAtlas {
-                layout: texture_atlas_layout,
+                layout: texture_atlas.0.clone().unwrap(),
                 index: 0,
             },
         ),
