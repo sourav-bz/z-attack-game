a6b444b37b1c2f397016f335f6e21769ebae83ff
sourav-bz <s.kumar001997@gmail.com>
2025-03-19 19:55:59 +0530

    gun sprite flipped added

--- src/animation.rs
+++ src/animation.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::{player::Player, CursorPosition, GameState};
+use crate::{gun::Gun, player::Player, CursorPosition, GameState};
 
 pub struct AnimationsPlugin;
 
@@ -11,7 +11,7 @@ impl Plugin for AnimationsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (animation_timer_tick, animate_player).run_if(in_state(GameState::InGame)),
+            (animation_timer_tick, animate_player, flip_gun_sprite).run_if(in_state(GameState::InGame)),
         );
     }
 }
@@ -45,6 +45,24 @@ fn animate_player(
             sprite.flip_x = false;
         }else{
             sprite.flip_x = true;
+        }
+    }
+}
+
+fn flip_gun_sprite(
+    cursor_position: Res<CursorPosition>,
+    mut gun_query: Query<(&mut Sprite, &Transform), With<Gun>>
+){
+    if gun_query.is_empty(){
+        return;
+    }
+
+    let (mut sprite, transform) = gun_query.single_mut();
+    if let Some(cursor_position) = cursor_position.0 {
+        if cursor_position.y < transform.translation.x {
+            sprite.flip_y = false;
+        }else{
+            sprite.flip_y = true;
         }
     }
 }
